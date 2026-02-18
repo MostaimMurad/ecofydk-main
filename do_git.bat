df1d4bb4f2bbce8da3dd144261c2f4cd8405b9a1
MostaimMurad <mostaimmurad@gmail.com>
2026-02-19 04:51:51 +0600

    chore: remove temp helper scripts

--- do_git.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "d:\Mostaim Dev\ecofydk-main"
-git status > "d:\Mostaim Dev\ecofydk-main\git_out.txt" 2>&1
-git add -A >> "d:\Mostaim Dev\ecofydk-main\git_out.txt" 2>&1
-git commit -m "feat: Add Admin Issue Tracker" >> "d:\Mostaim Dev\ecofydk-main\git_out.txt" 2>&1
-git push origin main >> "d:\Mostaim Dev\ecofydk-main\git_out.txt" 2>&1
-echo DONE >> "d:\Mostaim Dev\ecofydk-main\git_out.txt"
